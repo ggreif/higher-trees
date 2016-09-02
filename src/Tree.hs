3e4cb2f8c115f2be47a2cf809101c6d17b9d3cee
Gabor Greif <ggreif@gmail.com>
2016-09-02 23:50:26 +0200

    Some documentation and cleanups

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-} -- for Show only
 {-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE DeriveFunctor, DataKinds, GADTs #-}
+
 module Tree where
 
 --import Control.Comonad
@@ -17,20 +17,34 @@ data Tree a where
   Fork :: Tree a -> Tree a -> Tree a
  deriving Functor
 
+-- * A non-empty rose tree
+-- We'll want to map the top-dimensional part of our
+-- higher-dimensional trees to these.
+--
 data RTree a = RLeaf | a `RBranch` [RTree a] deriving (Eq, Show)
 
 --instance Comonad RTree where
   --extract
 
 -- * Higher tree
+-- For motivation see: http://opetopic.net/docs/hdts
 
+-- ** =Peano= naturals
+-- We need them for keeping track of the dimensions.
+-- Only to be used as a data kind.
+--
 data Peano = Z | S Peano
 
+-- ** Our higher-dimensional tree type
+-- =Points= (and only those) live in dimension zero
+-- the other critters live in higher dimensions
+--
 data HTree n a where
   Point :: a -> HTree Z a
   HLeaf :: HTree (S n) a
   Branch :: a -> HTree n (HTree (S n) a) -> HTree (S n) a
 
+-- *** =Show= ing
 instance Show a => Show (HTree Z a) where
   show (Point a) = "Point (" ++ show a ++ ")"
 
@@ -39,12 +53,13 @@ instance (Show a, Show (HTree n (HTree (S n) a))) => Show (HTree (S n) a) where
   show (a `Branch` t) = '(' : show a ++ " `Branch` " ++ show t ++ ")"
 
 
-
+-- *** Equating
 instance Eq a => Eq (HTree n a) where
   Point a == Point b = a == b
   HLeaf == HLeaf = True
   a `Branch` t == b `Branch` u = (a, t) == (b, u)
 
+-- *** Mapping
 instance Functor (HTree Z) where
   fmap f (Point a) = Point (f a)
 
@@ -52,6 +67,7 @@ instance Functor (HTree n) => Functor (HTree (S n)) where
   fmap _ HLeaf = HLeaf
   fmap f (a `Branch` t) = f a `Branch` fmap (fmap f) t
 
+-- *** A few specimen
 t2, t21, t213 :: HTree (S Z) Int
 t2 = 1 `Branch` Point HLeaf
 t21 = 1 `Branch` Point (2 `Branch` Point HLeaf)
